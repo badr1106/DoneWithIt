856248cbbe5a1b4a8d2312152ec03b6ddee2c490
Badr1106 <ahmed.badr@cgcegypt.com>
2023-03-28 12:16:00 +0200

    First Publish and change in app.json for description

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  console.log("App Exectuted");
-  
+export default function App() {  
   return (
     <View style={styles.container}>
       <Text>Hello React Native</Text>
