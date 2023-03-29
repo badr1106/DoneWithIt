dde4d49baa3767580d2a50eee1177e8ccf7f1c63
Badr1106 <ahmed.badr@cgcegypt.com>
2023-03-29 11:53:31 +0200

    Triggering Specific platform

--- App.js
+++ App.js
@@ -1,44 +1,18 @@
-import {
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  Image,
-  SafeAreaView,
-  Alert,
-  Button,
-} from "react-native";
+import { StatusBar } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Platform, Button } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={[containerStyle, styles.container]}>
-      <Text>Hello React Native</Text>
-      <TouchableHighlight onPress={() => console.log("Image Tapped")}>
-        <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-      <Button
-        title="Click Me"
-        onPress={() =>
-          Alert.prompt("Title", "Es7aa", (text) => console.log(text))
-        }
-        color="blue"
-      />
+    <SafeAreaView style={styles.container}>
+      <Button title="Click Me" onPress={() => console.log("Abyaaaaaaad")} />
     </SafeAreaView>
   );
 }
 
-const containerStyle = { backgroundColor: "orange" };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: Platform.OS === "android" ? "#fff" : "pink",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
