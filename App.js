4c8a709eb11c76a6b7799ad2092529aa3908c6d2
Badr1106 <ahmed.badr@cgcegypt.com>
2023-03-29 14:06:58 +0200

    Dividing app into flex boxes

--- App.js
+++ App.js
@@ -1,35 +1,31 @@
-import {
-  StyleSheet,
-  Dimensions,
-  StatusBar,
-  SafeAreaView,
-  Platform,
-  View,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
-  const orientation = useDeviceOrientation();
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
       <View
         style={{
           backgroundColor: "teal",
-          width: "100%",
-          height: orientation === "landscape" ? "100%" : "30%",
+          flex: 2,
         }}
-      ></View>
-    </SafeAreaView>
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          flex: 1,
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
