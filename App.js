f8d19a8ede3d2331dbf32ed8e4e4e6f5d8e42bc4
Badr1106 <ahmed.badr@cgcegypt.com>
2023-04-02 12:16:14 +0200

    Welcome - Image Screens on the app

--- App.js
+++ App.js
@@ -1,40 +1,7 @@
-import { View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center", // main
-        alignItems: "center", // secondary
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "teal",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute",
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  // return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+import colors from "../config/colors";
+
+function ViewImageScreen(props) {
+  return (
+    <View style={styles.imageContainer}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 15,
+    left: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 15,
+    right: 30,
+  },
+  imageContainer: {
+    backgroundColor: colors.black,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ViewImageScreen;
